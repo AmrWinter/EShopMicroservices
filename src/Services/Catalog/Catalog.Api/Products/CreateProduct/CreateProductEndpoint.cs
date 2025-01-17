31b2f9d1c412656fc31b523f0d2da36b8c7fbed2
Amr Sheta <Amr.Winter1@gmail.com>
2025-01-18 01:22:31 +0200

    create post is ready with dummy db connection, not real connection.

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,23 @@
 ﻿namespace Catalog.Api.Products.CreateProduct;
 
-public class CreateProductEndpoint
+public record CreateProductRequest(string Name, string Description, string Category, string ImageFile, decimal Price);
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint: ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Creates a new product")
+        .WithDescription("Creates a new product with the provided name, description, category, image file, and price.");
+    }
 }
