08b181cb0919926a349467bdf8ca278c8c17035a
Amr Sheta <Amr.Winter1@gmail.com>
2025-01-18 00:08:18 +0200

    Make Catalog Api depends on BuildingBlocks project

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record class CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price)
+    : ICommand<CreateProductCommandResponse>;
+public record class CreateProductCommandResponse(Guid Id);
+
+internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand, CreateProductCommandResponse>
+{
+    public async Task<CreateProductCommandResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
